d71481bb4b96eff92811ac4692ca54b7100c1b64
nirojan-sys <68774552+nirojan-sys@users.noreply.github.com>
2022-05-26 18:59:57 +0200

    with std_logic
    all files updated with std_logic
    multiplier has some issues with l.s.b

---
+++ Hardware Engineering Lab/Project files using std_logic/project calculator/8_bit_multiplier_TB.vhd
@@ -0,0 +1,31 @@
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity e8_bit_multiplier_TB is 
+end entity;
+
+architecture behavioral of e8_bit_multiplier_TB is
+
+signal A_TB,B_TB  : std_logic_vector(7 downto 0);
+signal Ans_TB     : std_logic_vector (15 downto 0) ;
+
+
+component  e8_bt_multiplier is
+
+port(x : in  std_logic_vector (7 downto 0);     --:= ?10101010?;
+     y : in  std_logic_vector(7 downto 0);     --:= ?10101010?;
+     Ans : out  std_logic_vector(15 downto 0));
+
+end component;
+
+
+
+begin 
+uut:   e8_bt_multiplier port map(A_TB,B_TB,Ans_TB);
+       A_TB<="00101001","00100010" after 10ns;
+       B_TB<="00100010","00101101" after 10ns;
+--A_TB<="00110010","00010101" after 10ns;
+--B_TB<="00101010","00001101" after 10ns;
+
+end behavioral;
