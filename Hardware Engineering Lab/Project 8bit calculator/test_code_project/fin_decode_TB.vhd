90793ff4c730e821c5f049a0935e6324d9584f82
nirojan-sys <68774552+nirojan-sys@users.noreply.github.com>
2022-06-09 16:43:35 +0200

    report  statement included
    test bench with report statement updated

--- Hardware Engineering Lab/Project 8bit calculator/test_code_project/fin_decode_TB.vhd
+++ Hardware Engineering Lab/Project 8bit calculator/test_code_project/fin_decode_TB.vhd
@@ -8,13 +8,16 @@ end entity;
 
 architecture behavioral of fin_decode_TB is
 
-signal x_TB,y_TB  : std_logic_vector(7 downto 0);
 signal sel_TB1 : bit_vector(1 downto 0);
+signal x_TB,y_TB  : std_logic_vector(7 downto 0);
+
 signal input_TB :std_logic_vector(15 downto 0);
 signal ones_TB,tens_TB,hundreds_TB,thousands_TB: std_logic_vector (3 downto 0);
 signal LED_out_1_TB,LED_out_2_TB,LED_out_3_TB,LED_out_4_TB : std_logic_vector(6 downto 0 );
 
 
+signal add_result_test :std_logic_vector(15 downto 0);
+signal add_result_bit: bit;
 component  bcdm is
 
 port (x : in  std_logic_vector(7 downto 0); 
@@ -33,13 +36,38 @@ end component;
 
 
 begin 
-uut1:   bcdm port map(x_TB,y_TB,sel_TB1,input_TB,ones_TB,tens_TB,hundreds_TB,thousands_TB,LED_out_1_TB,LED_out_2_TB,LED_out_3_TB,LED_out_4_TB);
-        x_TB<="00011110","00111100" after 100ns;  -----testing  x=30 , x=60
-       y_TB<="00010100","00101000" after 100ns;  ----- testing y=20 , y=40
+uut1:   bcdm 
+        port map(x_TB,y_TB,sel_TB1,input_TB,ones_TB,tens_TB,hundreds_TB,thousands_TB,LED_out_1_TB,LED_out_2_TB,LED_out_3_TB,LED_out_4_TB);
+add_result_test<=input_TB;
+
+
+STIMULUS :process
+begin
+
+        x_TB<="00011110";--,"00111100" after 100ns;  -----testing  x=30 , x=60
+        y_TB<="00010100";--,"00101000" after 100ns;  ----- testing y=20 , y=40
       
+        
+
+        assert (add_result_bit='0') report "passed test for calculation" severity NOTE; --- indicates 30-20=10 is a correct answer
+        assert (add_result_bit='1') report "Failed test for calculation" severity FAILURE; --- indicates answer of 30-20 is not 10 -a wrong answer
+        
+
+
        --sel_TB1<="00","01" after 50ns,"10" after 75ns,"11" after 100ns;
        
-       --A_TB<="00110010","00010101" after 10ns;
-       --B_TB<="00101010","00001101" after 10ns;
+      wait for 50ns;
+       
+end process;
+
+process(add_result_test)
+begin
+ case add_result_test is
+    when "0000000000001010" => add_result_bit <= '1'; --10
+    when "0000000000110010" => add_result_bit <= '1'; --50
+    when "0000001001011000" => add_result_bit <= '1'; --600   
+    when others =>add_result_bit <= '0';
+  end case;
+end process;
 
 end behavioral;
