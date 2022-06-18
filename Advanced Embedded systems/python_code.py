b419301ea10871bab3920bbecfcbbdfd3194145a
Nirojan Navaratnarajah <68774552+Nirojan-Navaratnarajah@users.noreply.github.com>
2022-06-18 02:07:07 +0200

    Update python_code.py

--- Advanced Embedded systems/python_code.py
+++ Advanced Embedded systems/python_code.py
@@ -5,26 +5,27 @@ import paho.mqtt.client as mqtt
 
 import time
 import os
-
+import random
 
 command =""
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
-    client.subscribe("esp32/ultra")
-    client.subscribe("esp32/buzzer")
+   # client.subscribe("esp32/temp")
+    #client.subscribe("esp32/moisture")
+    client.subscribe("esp32/water")
 
-#    client.publish('esp32/output', on)
-#    if(command== "Hand near sensor detected"):
-    client.publish("esp32/output", "on")
- #       print("sending on command to activate buzzer")
+
+
+
+
 
 def on_message(client, userdata, msg):
     global command
     command = msg.payload.decode()
     print(   command)
-    if(command== "Hand near sensor detected"):
-        client.publish("esp32/output", "on")
-        print("sending on command to activate buzzer")
+    if(command=="on"):
+        client.publish("esp32/motor", "on") # automatically turning on the motor when moisture level goes down
+        print("sending on command to activate motor")
   #client.disconnect()
 
 client = mqtt.Client()
@@ -34,10 +35,27 @@ client.on_connect = on_connect
 client.on_message = on_message
 
 client.loop_start()
+#-------------------------------------------------------------------------------
+
+
+
+#client.publish('esp32/output', on)
+
+
+ #-------------------------------------------------------------
+#client.publish('esp32/output', on)
+
 
 while True:
-     if(command== "hand near sensor"):
-        client.publish("esp32/output", "on")
-        print("sending on command to activate buzzer")
+     if(command=="turn on"):
+        client.publish("esp32/motor", "on")
+        print("sending on command to activate motor")
+
+
+        #if (client.on_message):
+         #   print('recieved distance')
+#            print(msg)
+#         time.sleep(0.2)
+
         continue
 
